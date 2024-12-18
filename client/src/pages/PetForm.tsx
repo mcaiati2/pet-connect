0eeaa3aeea6f35d303c0e982c08c623a9e528c45
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-12-18 10:26:51 -0500

    styling for remaining pages added

--- client/src/pages/PetForm.tsx
+++ client/src/pages/PetForm.tsx
@@ -9,7 +9,7 @@ import { GET_USER_PETS } from '../graphql/queries';
 const initialFormData = {
   name: '',
   type: '',
-  age: 0,
+  age: '',
   errorMessage: ''
 };
 
@@ -48,38 +48,41 @@ function PetForm() {
   }
 
   return (
-    <Container>
-      <Form onSubmit={handleSubmit} style={{ width: '500px' }} className="mx-auto mt-5">
-        <h2 className="text-center mt-3">Create Pet</h2>
+    <div className="dashboard">
+      <div className="overlay">
+        <Container>
+          <Form onSubmit={handleSubmit} style={{ width: '500px' }} className="mx-auto mt-5">
+            <h2 className="text-center mt-3 color-white">Create Pet</h2>
 
-        {formData.errorMessage && (
-          <p className="text-center text-danger">{formData.errorMessage}</p>
-        )}
+            {formData.errorMessage && (
+              <p className="text-center text-danger">{formData.errorMessage}</p>
+            )}
+            <Form.Group className="mb-3">
+              <Form.Label className="color-white">Type</Form.Label>
+              <Form.Control name="type" onChange={handleInputChange} value={formData.type} type="text" placeholder="What type of pet are you adding?" />
+            </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Name</Form.Label>
-          <Form.Control name="name" onChange={handleInputChange} value={formData.name} type="text" placeholder="Enter the pet's name" />
-        </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label className="color-white">Name</Form.Label>
+              <Form.Control name="name" onChange={handleInputChange} value={formData.name} type="text" placeholder="Enter your pet's name" />
+            </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Type</Form.Label>
-          <Form.Control name="type" onChange={handleInputChange} value={formData.type} type="text" placeholder="Enter the pet's type" />
-        </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Enter the Pet's Age</Form.Label>
-          <Form.Control name="age" onChange={handleInputChange} value={formData.age} type="number" />
-        </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label className="color-white">Enter your pet's age</Form.Label>
+              <Form.Control name="age" onChange={handleInputChange} value={formData.age} type="number" placeholder="Enter your pet's age" />
+            </Form.Group>
 
-        <div className="d-grid gap-2">
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </div>
-      </Form>
-    </Container>
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Container>
+      </div>
+    </div>
   )
 }
 
 export default PetForm;
-
